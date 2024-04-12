23dec24f7e46eda23d1872ba8773709be5c679f5
lk432963 <lk432963@alibaba-inc.com>
2024-04-12 16:35:18 +0800

    adjust docker env

--- env/script.bash
+++ env/script.bash
@@ -1,7 +1,9 @@
 ##docker
-docker pull nvidia/cuda:12.3.2-devel-centos7
-docker run -it -v /home/lk432963/code/llmsys:/llmsys --name=centos7-CUDA --gpus all nvidia/cuda:12.3.2-devel-centos7 /bin/bash
+docker pull nvidia/cuda:11.0.3-cudnn8-devel-centos7
+docker run -it -v /home/lk432963/code/:/code --name=centos7-CUDA --gpus all nvidia/cuda:11.0.3-cudnn8-devel-centos7 /bin/bash
 docker exec -it centos7-CUDA /bin/bash
 
 ##conda
 wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh
+export PATH="/root/anaconda3/bin:$PATH"
